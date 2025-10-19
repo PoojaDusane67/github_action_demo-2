10c2a10426218f65cecfaa9e8b0a2bab6c798e07
Pooja Dusane <dusanepooja91@gmail.com>
2025-10-20 00:50:08 +0530

    test 6 install dependencies

--- transform.py
+++ transform.py
@@ -1,1 +1,12 @@
+import pandas as pd 
+
 print("the data is transformed")
+
+data = {
+    "id":[1,2,3,4,5],
+    "name":["daya","jetha","tapu","sonu","gogi"],
+    "age":[50,40,30,20,10]
+}
+
+df=pd.DataFrame(data)
+print(df)
